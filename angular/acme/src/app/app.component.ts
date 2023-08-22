5d9ad39cd06571ce9eda61bb867973f4f8ddd424
Washington Acero M <shin321654987@gmail.com>
2023-08-21 19:20:32 -0700

    aded code for show data

--- angular/acme/src/app/app.component.ts
+++ angular/acme/src/app/app.component.ts
@@ -1,18 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { WeatherForecastService } from './proxy/acme/api-host/controllers';
+import { WeatherForecast } from './proxy/acme/api-host';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'acme';
-  constructor(service: WeatherForecastService) {
-    service.get().subscribe(a => {
-      // remote data
-      console.log("a");
-      console.log(a);
+  data:WeatherForecast[] = []
+  constructor(private service: WeatherForecastService) {
+  }
+  ngOnInit(): void {
+    this.service.get().subscribe(a => {
+      this.data = a
     })
   }
 }
