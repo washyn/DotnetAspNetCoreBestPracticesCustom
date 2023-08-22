5655f8a941ef23d15f161657b97d22627059b327
Washington Acero M <shin321654987@gmail.com>
2023-08-21 18:58:55 -0700

    installder angukar cdk and material

--- angular/acme/src/app/app.module.ts
+++ angular/acme/src/app/app.module.ts
@@ -4,6 +4,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { AbpCustomModule } from './abp-custom.module';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [
@@ -12,7 +13,8 @@ import { AbpCustomModule } from './abp-custom.module';
   imports: [
     BrowserModule,
     AbpCustomModule.forRoot(),
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
