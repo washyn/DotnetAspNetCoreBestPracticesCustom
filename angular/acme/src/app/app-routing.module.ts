dbed4422b341e7ac04a029d495d7b17a00772d3b
Washington Acero M <shin321654987@gmail.com>
2023-09-22 19:47:17 -0700

    aded routes for components

--- angular/acme/src/app/app-routing.module.ts
+++ angular/acme/src/app/app-routing.module.ts
@@ -1,10 +1,36 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { DashboardDemoComponent } from './module/dashboard-demo/dashboard-demo.component';
+import { AddressFormDemoComponent } from './module/address-form-demo/address-form-demo.component';
+import { DragDropDemoComponent } from './module/drag-drop-demo/drag-drop-demo.component';
+import { TableDemoComponent } from './module/table-demo/table-demo.component';
+import { TreeDemoComponent } from './module/tree-demo/tree-demo.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'dashboard',
+    component: DashboardDemoComponent,
+  },
+  {
+    path: 'address-form',
+    component: AddressFormDemoComponent,
+  },
+  {
+    path: 'drag-drop',
+    component: DragDropDemoComponent,
+  },
+  {
+    path: 'table',
+    component: TableDemoComponent,
+  },
+  {
+    path: 'tree',
+    component: TreeDemoComponent,
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
