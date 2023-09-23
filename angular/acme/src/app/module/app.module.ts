29147dccaa2e6f0e45bcaede38f23d22b46656fc
Washington Acero M <shin321654987@gmail.com>
2023-09-22 17:41:18 -0700

    aded sample of module

---
+++ angular/acme/src/app/module/app.module.ts
@@ -0,0 +1,11 @@
+import { CommonModule } from '@angular/common';
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+@NgModule({
+  declarations: [],
+  imports: [CommonModule, BrowserModule],
+  providers: [],
+  bootstrap: [],
+})
+export class AppModule {}
